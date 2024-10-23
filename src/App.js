f14ea51bbf5825f9857d91118591529a46505e1b
cloud178 <denis-biryukov-13@mail.ru>
2024-10-23 17:57:38 +0300

    lesson 9 separate components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./header";
+import Technologies from "./technologies";
+
 
 const App = () => {
     return (
@@ -8,27 +11,6 @@ const App = () => {
             <Technologies/>
         </div>
     );
-}
-
-const Technologies = () => {
-    return (
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
-    )
-}
-
-const Header = () => {
-    return (
-        <div>
-            <a href="#s">Home</a>
-            <a href="#s">News Feed</a>
-            <a href="#s">Messages</a>
-        </div>
-    )
 }
 
 export default App;
